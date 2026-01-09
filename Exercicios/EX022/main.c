fcdd91eee5dc918bf431266a37954a70d25d963f
Thais Ribeiro <thais.ribeiroramos.dev@gmail.com>
2026-01-09 13:48:30 -0400

    Resolução: EX022

---
+++ Exercicios/EX022/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Número Primo
+
+int main() {
+	
+	int num, i, div;
+	
+	printf (" - Informe um numero: ");
+	scanf ("%d", &num);
+	
+	while (num < 1) {
+		printf ("---------------------------------\n");
+		printf (" [!] O numero deve ser maior que 0.");
+		scanf ("%d", &num);
+	}
+	
+	div = 0;
+	for (i = 1; i <= num; i++) {
+		if (num % i == 0) {
+			div++;
+		}
+	}
+	
+	printf ("---------------------------------\n");
+	if (div == 2) {
+		printf (" - %d e primo", num);
+	}
+	else {
+		printf (" - %d NAO e primo", num);
+	}
+	
+	return 0;
+}
